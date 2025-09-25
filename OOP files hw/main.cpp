58149e3a2c75066a030a7078a6a65c32972048c4
ysantonyance <zinovevakarina569@gmail.com>
2025-09-25 05:46:49 +0300

    version 1

---
+++ OOP files hw/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void main() {
+    ofstream outFile("C://files//cities.txt");
+    if (!outFile) {
+        cout << "error!\n";
+        return;
+    }
+
+    string city;
+    for (int i = 1; i <= 10; ++i) {
+        outFile << "enter the name of the city " << i << ": ";
+        getline(cin, city);
+        outFile << city << "\n";
+    }
+    outFile.close();
+
+    ifstream inFile("C://files//cities.txt");
+    if (!inFile) {
+        cout << "error!\n";
+        return;
+    }
+
+    string line;
+    while (getline(inFile, line)) {
+        cout << line << "\n";
+    }
+    inFile.close();
+}
