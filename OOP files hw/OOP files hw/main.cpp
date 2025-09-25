8c0adfcfaf01b63a4f4e0423331c6701e9879bf8
ysantonyance <149514654+ysantonyance@users.noreply.github.com>
2025-09-25 05:36:41 +0300

    робота з файлами в ООП стилі

---
+++ OOP files hw/OOP files hw/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void main() {
+    ofstream outFile("C://files//cities.txt");
+    if (!outFile) {
+        cout << "error!\n";
+        return;
+    }
+
+    string city;
+    for (int i = 1; i <= 10; ++i) {
+        //outFile << "number " << i << ": " << "\n";
+        outFile << "enter the name of the city " << i << ": ";
+        getline(cin, city);
+        outFile << city << "\n";
+    }
+    outFile.close();
+
+    ifstream inFile("C://files//cities.txt");
+    if (!inFile) {
+        cout << "error!\n";
+        return;
+    }
+
+    string line;
+    while (getline(inFile, line)) {
+        cout << line << "\n";
+    }
+    inFile.close();
+}
